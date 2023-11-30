6648265d541ab66013dcc2982f75a6e4c86090c8
austin-hoover <hooveram@ornl.gov>
2023-11-30 11:39:38 -0500

    Add convenience function to load model, config, checkpoints from folder

--- experiments/setup.py
+++ experiments/setup.py
@@ -1,4 +1,8 @@
 """Tools for loading model."""
+import os
+import pickle
+import sys
+
 import torch
 import torch.nn as nn
 import zuko
@@ -52,3 +56,51 @@ def get_step_and_iteration_number(checkpoint_filename):
     checkpoint_filename = checkpoint_filename.split(".pt")[0]
     (step, iteration) = [int(string) for string in checkpoint_filename.split("_")[-2:]]
     return step, iteration
+
+
+def load_info(path):
+    """Load text file with command line arguments as dict."""
+    info = {}
+    file = open(path, "r")
+    for line in file:
+        line = line.rstrip()
+        if line.startswith("Namespace"):
+            line = line.split("Namespace(")[1].split(")")[0]
+            strings = [string.strip() for string in line.split(",")]
+            for string in strings:
+                key, val = string.split("=")
+                info[key] = val
+    file.close()
+    return info
+
+
+def load_run(folder):
+    """Load model, checkpoints, and config.."""
+    
+    # Load config dict.
+    path = os.path.join(folder, "config.pkl")
+    with open(path, "rb") as file:
+        cfg = pickle.load(file)
+    
+    # Load command line arguments.
+    path = os.path.join(folder, "log.txt")
+    info = load_info(path)
+    
+    # Set up the model.
+    model = setup_model(cfg)
+    model.eval()
+    
+    # Load checkpoint paths.
+    checkpoints = []
+    subdir = os.path.join(folder, "checkpoints")
+    checkpoint_paths = [os.path.join(subdir, f) for f in sorted(os.listdir(subdir))]
+    for checkpoint_path in checkpoint_paths:
+        (step, iteration) = get_step_and_iteration_number(checkpoint_path)
+        checkpoint = {
+            "step": step,
+            "iteration": iteration,
+            "path": checkpoint_path,
+        }
+        checkpoints.append(checkpoint)
+        
+    return (model, checkpoints, cfg, info)
