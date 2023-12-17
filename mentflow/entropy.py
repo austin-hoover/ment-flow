617a6e35a91874bf7a4b1142b8cbfb42fccbd0e1
austin-hoover <hooveram@ornl.gov>
2023-12-17 16:09:55 -0500

    Add monte carle entropy estimator class
    All entropy estimators now take x and log_prob as inputs

--- mentflow/entropy.py
+++ mentflow/entropy.py
@@ -1,19 +1,18 @@
-"""Methods to estimate entropy from samples."""
+"""Methods to estimate entropy from samples and/or probability density."""
 import typing
 from typing import Optional
 
 import numpy as np
 import torch
-import torch.nn as nn
 
 
-class EntropyEstimator(nn.Module):
-    """Estimates entropy from samples."""
+class EntropyEstimator(torch.nn.Module):
+    """Estimates negative entropy from samples and/or log probability."""
     def __init__(self, prior=None) -> None:
         super().__init__()
         self.prior = prior
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, log_prob: torch.Tensor = None) -> torch.Tensor:
         raise NotImplementedError
 
 
@@ -22,31 +21,43 @@ class EmptyEntropyEstimator(EntropyEstimator):
     def __init__(self, prior=None):
         super().__init__(prior=prior)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, log_prob: torch.Tensor = None) -> torch.Tensor:
         return 0.0
 
 
 class CovarianceEntropyEstimator(EntropyEstimator):
-    """Estimates entropy from covariance matrix."""
+    """Estimates negative entropy from covariance matrix."""
     def __init__(self, prior=None, pad=1.00e-12):
         if prior is not None:
             raise ValueError("This class cannot estimate relative entropy (prior != None).")
         super().__init__(prior=prior)
         self.pad = pad
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, log_prob: torch.Tensor = None) -> torch.Tensor:
         eps = torch.sqrt(torch.det(torch.cov(x.T)))
         H = -3.0 * np.log(2.0 * np.pi * np.e) - torch.log(eps + self.pad)
         return H
 
 
 class KNNEntropyEstimator(EntropyEstimator):
-    """Estimates entropy from k nearest neighbors."""
+    """Estimates negative entropy from k nearest neighbors."""
     def __init__(self, prior=None, k=5):
         if prior is not None:
             raise ValueError("This class cannot estimate relative entropy (prior != None).")
         super().__init__(prior=prior)
         self.k = k
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, log_prob: torch.Tensor = None) -> torch.Tensor:
         raise NotImplementedError
+
+
+class MonteCarloEntropyEstimator(EntropyEstimator):
+    """Estimates negative entropy using Monte Carlo integral approximation."""
+    def __init__(self, prior=None):
+        super().__init__(prior=prior)
+
+    def forward(self, x: torch.Tensor, log_prob: torch.tensor) -> torch.Tensor:
+        H = torch.mean(log_prob)
+        if self.prior is not None:
+            H = H - torch.mean(self.prior.log_prob(x))
+        return H
