4225eb8c04bd926d7539bad64fa7decc99e8b0e2
austin-hoover <hooveram@ornl.gov>
2024-01-14 22:37:25 -0500

    Hardcode multipole transform for now

--- mentflow/sim/transform.py
+++ mentflow/sim/transform.py
@@ -99,7 +99,6 @@ class MultipoleTransform(Transform):
         self.skew = skew
         
     def forward(self, X):
-        k = self.strength / np.math.factorial(self.order - 1)
 
         U = X.clone()
         
@@ -109,18 +108,42 @@ class MultipoleTransform(Transform):
         else:
             y = 0.0 * X[:, 0]
 
-        z = torch.complex(x, y)
-        zn = torch.pow(z, float(self.order - 1))
-        zn = z ** (self.order - 1)
-        
-        if self.skew:
-            U[:, 1] = X[:, 1] + k * zn.imag
-            if X.shape[1] > 2:
-                U[:, 3] = X[:, 3] + k * zn.real
+        ## MPS does not support complex.        
+        # z = torch.complex(x, y)
+        # zn = torch.pow(z, float(self.order - 1))
+        # zn = z ** (self.order - 1)
+        # zn_imag = zn.imag
+        # zn_real = zn.real
+
+        ## Hard-code a few values of n for now.
+        if self.order == 1:
+            zn_real = 1.0
+            zn_imag = 1.0
+        if self.order == 2:
+            zn_real = x
+            zn_imag = y
+        if self.order == 3:
+            zn_real = (x**2) - (y**2)
+            zn_imag = (2.0 * x * y)
+        elif self.order == 4:
+            zn_real = (+x**3) - (3.0 * y**2 * x)
+            zn_imag = (-y**3) + (3.0 * x**2 * y)
+        elif self.order == 5:
+            zn_real = (x**4) - (6.0 * x**2 * y**2) + (y**4)
+            zn_imag = (4.0 * x**3 * y) - (4.0 * x * y**3)
         else:
-            U[:, 1] = X[:, 1] - k * zn.real
+            raise ValueError("MPS-compatible MultipoleTransform requires order <= 5.")
+
+        
+        k = self.strength / np.math.factorial(self.order - 1)
+        if self.skew:
+            U[:, 1] = X[:, 1] + k * zn_imag
             if X.shape[1] > 2:
-                U[:, 3] = X[:, 1] + k * zn.imag
+                U[:, 3] = X[:, 3] + k * zn_real
+        else:
+            U[:, 1] = X[:, 1] - k * zn_real
+            if X.shape[1] > 2:
+                U[:, 3] = X[:, 1] + k * zn_imag
         return U
         
 
