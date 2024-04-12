211e418b1803cab2e181fd68c8f8116773accf97
austin-hoover <hooveram@ornl.gov>
2024-04-11 20:43:58 -0400

    Update sample.py

--- mentflow/sample.py
+++ mentflow/sample.py
@@ -8,7 +8,8 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-from mentflow.utils.grid import get_grid_points
+from mentflow.utils import coords_from_edges
+from mentflow.utils import get_grid_points
 
 
 def tqdm_wrapper(iterable, verbose=False):
@@ -19,277 +20,91 @@ def random_uniform(lb: float, ub: float, size: int, device=None) -> torch.Tensor
     return lb + (ub - lb) * torch.rand(size, device=device)
 
 
-def random_choice(items: torch.tensor, n: int, pdf: torch.Tensor):
-    return items[pdf.multinomial(num_samples=n, replacement=True)]
+def random_choice(items: torch.tensor, size: int, p: torch.Tensor):
+    return items[p.multinomial(num_samples=size, replacement=True)]
 
 
-def sample_hist_bins(hist: torch.Tensor, n: int) -> torch.Tensor:
-    pdf = torch.ravel(hist)
+def sample_hist_bins(hist: torch.Tensor, size: int) -> torch.Tensor:
+    pdf = torch.ravel(hist) + 1.00e-15
     idx = torch.squeeze(torch.nonzero(pdf))
-    pdf = pdf[idx]    
-    pdf_sum = torch.sum(pdf)    
-    if pdf_sum > 0.0:
-        pdf = pdf / pdf_sum
-    idx = random_choice(idx, n, pdf=pdf)
-    idx = torch.unravel_index(idx, hist.shape)
+    idx = random_choice(idx, size, p=(pdf / torch.sum(pdf)))
     return idx
 
 
 def sample_hist(
     hist: torch.Tensor,
-    bin_edges: List[torch.Tensor] = None,
-    n: int = 1,
+    edges: List[torch.Tensor],
+    size: int,
     noise: float = 0.0,
-    device=None,
-):
-    d = hist.ndim
+    device: torch.device = None,
+) -> torch.Tensor:
+    ndim = hist.ndim
+    if ndim == 1:
+        edges = [edges]
 
-    if bin_edges is None:
-        bin_edges = [torch.arange(hist.shape[axis] + 1) for axis in range(d)]
+    idx = sample_hist_bins(hist, size)
+    idx = torch.unravel_index(idx, hist.shape)
 
-    if d == 1 and len(bin_edges) > 1:
-        bin_edges = [bin_edges]
-
-    idx = sample_hist_bins(hist, n)
-
-    x = torch.zeros(n, d, device=device)
-    for axis in range(d):
-        lb = bin_edges[axis][idx[axis]]
-        ub = bin_edges[axis][idx[axis] + 1]
-        x[:, axis] = random_uniform(lb, ub, n, device=device)
+    x = torch.zeros((size, ndim), device=device)
+    for axis in range(ndim):
+        lb = edges[axis][idx[axis]]
+        ub = edges[axis][idx[axis] + 1]
+        x[:, axis] = random_uniform(lb, ub, size, device=device)
         if noise:
             delta = ub - lb
-            x[:, axis] += noise * delta * (torch.rand(n, device=device) - 0.5)
+            x[:, axis] += 0.5 * random_uniform(-delta, delta, size, device=device)
     x = torch.squeeze(x)
     return x
-
-
-def resample_hist(hist, n):
-    """Equivalent to sampling particles from histogram and recomputing histogram."""
-    pdf = hist.ravel()
-    idx = torch.squeeze(torch.nonzero(pdf))
-    pdf = pdf[idx]
-    pdf = pdf / torch.sum(pdf)
-    idx = random_choice(idx, n, pdf=pdf)
-    idx = idx.float()
-    bin_edges = torch.arange(hist.numel() + 1).float() - 0.5
-    hist_out = torch.histogram(idx, bins=bin_edges, density=False)
-    hist_out = hist_out.hist
-    hist_out = torch.reshape(hist_out, hist.shape)
-    return hist_out
 
 
 class GridSampler:
     def __init__(
         self,
-        grid_limits: List[Tuple[float]],
-        grid_shape: Tuple[int],
+        limits: List[Tuple[float]],
+        shape: Tuple[int],
         noise: float = 0.0,
-        device=None,
-        store=True,
-    ):
+        device: torch.device = None,
+        store: bool = True,
+    ) -> None:
         self.device = device
-        self.grid_shape = grid_shape
-        self.grid_limits = grid_limits
-        self.d = len(grid_limits)
+        self.shape = shape
+        self.limits = limits
+        self.ndim = len(limits)
         self.noise = noise
-        self.grid_edges = [
-            torch.linspace(
-                self.grid_limits[axis][0],
-                self.grid_limits[axis][1],
-                self.grid_shape[axis] + 1,
-            )
-            for axis in range(self.d)
-        ]
-        self.grid_coords = [0.5 * (e[:-1] + e[1:]) for e in self.grid_edges]
-        self.grid_points = None
         self.store = store
 
-    def send(self, x):
+        self.edges = [
+            torch.linspace(
+                self.limits[axis][0],
+                self.limits[axis][1],
+                self.shape[axis] + 1,
+            )
+            for axis in range(self.ndim)
+        ]
+        self.coords = [coords_from_edges(e) for e in self.edges]
+        self.points = None
+
+    def send(self, x: torch.Tensor) -> torch.Tensor:
         return x.type(torch.float32).to(self.device)
 
-    def get_mesh(self) -> Tuple[torch.Tensor]:
-        return torch.meshgrid(*self.grid_coords, indexing="ij")
-
     def get_grid_points(self) -> torch.Tensor:
-        if self.grid_points is not None:
-            return self.grid_points
-        grid_points = torch.vstack([C.ravel() for C in self.get_mesh()]).T
-        grid_points = self.send(grid_points)
+        if self.points is not None:
+            return self.points
+        points = get_grid_points(*self.coords)
+        points = self.send(points)
         if self.store:
-            self.grid_points = grid_points
-        return grid_points
+            self.points = points
+        return points
 
-    def __call__(self, prob_func: Callable, n: int) -> torch.Tensor:
-        grid_points = self.get_grid_points()
-        grid_points = self.send(grid_points)
-        prob = prob_func(grid_points)
-        prob = torch.reshape(prob, self.grid_shape)
-        x = sample_hist(prob, bin_edges=self.grid_edges, n=n, noise=self.noise, device=self.device)
+    def __call__(self, prob_func: Callable, size: int) -> torch.Tensor:
+        prob = prob_func(self.get_grid_points())
+        prob = torch.reshape(prob, self.shape)
+        x = sample_hist(prob, self.edges, size=size, noise=self.noise, device=self.device)
         x = self.send(x)
         return x
 
     def to(self, device):
         self.device = device
-        self.grid_edges = [self.send(e) for e in self.grid_edges]
-        self.grid_coords = [self.send(c) for c in self.grid_coords]
+        self.edges = [self.send(e) for e in self.edges]
+        self.coords = [self.send(c) for c in self.coords]
         return self
-
-
-class SliceGridSampler:
-    def __init__(
-        self,
-        grid_limits: List[Tuple[float]],
-        grid_shape: Tuple[int],
-        proj_dim: int = 2,
-        int_size: int = 10000,
-        int_method: str = "grid",
-        int_batches: int = 1,
-        noise: float = 0.0,
-        verbose: bool = False,
-        device=None,
-    ):
-        self.device = device
-        self.grid_shape = grid_shape
-        self.grid_limits = grid_limits
-        self.dim = len(grid_limits)
-        self.proj_dim = proj_dim
-        self.samp_dim = self.dim - self.proj_dim
-        self.noise = noise
-        self.verbose = verbose
-
-        self.grid_edges = [
-            torch.linspace(
-                self.grid_limits[axis][0],
-                self.grid_limits[axis][1],
-                self.grid_shape[axis] + 1,
-            )
-            for axis in range(self.dim)
-        ]
-        self.grid_coords = [0.5 * (e[:-1] + e[1:]) for e in self.grid_edges]
-
-        # Projection grid.
-        self.proj_axis = tuple(range(self.proj_dim))
-        self.proj_grid_shape = self.grid_shape[: self.proj_dim]
-        self.proj_grid_edges = self.grid_edges[: self.proj_dim]
-        self.proj_grid_coords = self.grid_coords[: self.proj_dim]
-        self.proj_grid_points = get_grid_points(*self.proj_grid_coords)
-
-        # Sampling grid.
-        self.samp_axis = tuple(range(self.proj_dim, self.dim))
-        self.samp_grid_shape = self.grid_shape[self.proj_dim :]
-        self.samp_grid_edges = self.grid_edges[self.proj_dim :]
-        self.samp_grid_coords = self.grid_coords[self.proj_dim :]
-        self.samp_grid_points = get_grid_points(*self.samp_grid_coords)
-
-        # Integration limits (integration axis = sampling axis).
-        self.int_size = int_size
-        self.int_method = int_method
-        self.int_batches = int_batches
-        self.int_limits = self.grid_limits[self.proj_dim :]
-        self.int_axis = self.samp_axis
-        self.int_dim = self.samp_dim
-
-        # We will evaluate the function on the sampling grid. The first `proj_dim`
-        # dimensions are the projected coordinates.
-        self.eval_points = torch.zeros((math.prod(self.samp_grid_shape), self.dim))
-        self.eval_points[:, self.samp_axis] = get_grid_points(*self.samp_grid_coords)
-
-    def to(self, device):
-        self.device = device
-        self.grid_edges = [self.send(e) for e in self.grid_edges]
-        self.grid_coords = [self.send(c) for c in self.grid_coords]
-        return self
-
-    def send(self, x):
-        return x.type(torch.float32).to(self.device)
-
-    def project(self, func: Callable) -> torch.Tensor:
-        """Project function onto onto lower dimensional plane."""
-
-        x = torch.zeros((self.int_size, self.dim))
-
-        def set_int_points(x):
-            if self.int_method == "grid":
-                int_res = self.int_size ** (1.0 / self.int_dim)
-                int_res = math.ceil(int_res)
-                int_res = int(int_res)
-                int_coords = [
-                    torch.linspace(xmin, xmax, int_res)
-                    for (xmin, xmax) in self.int_limits
-                ]
-                int_points = get_grid_points(*int_coords)
-                x[:, self.int_axis] = int_points[: x.shape[0], :]
-            elif self.int_method == "uniform":
-                for axis, (xmin, xmax) in zip(self.int_axis, self.int_limits):
-                    x[:, axis] = random_uniform(xmin, xmax, self.int_size, device=self.device)
-            elif self.int_method == "gaussian":
-                scale = [xmax for (xmin, xmax) in self.int_limits]
-                for axis, (xmin, xmax) in zip(self.int_axis, self.int_limits):
-                    x[:, axis] = 0.75 * (xmax - xmin) * torch.randn(self.int_size)
-            else:
-                raise ValueError("Invalid int_method")
-            return x
-
-        rho = torch.zeros(self.proj_grid_points.shape[0])
-        for _ in range(self.int_batches):
-            x = set_int_points(x)
-            for i in tqdm_wrapper(range(rho.shape[0]), self.verbose):
-                x[:, self.proj_axis] = self.proj_grid_points[i, :]
-                rho[i] += torch.sum(func(x))
-        rho = torch.reshape(rho, self.proj_grid_shape)
-        return rho
-
-    def __call__(self, prob_func: Callable, n: int) -> torch.Tensor:
-        # Compute projection.
-        if self.verbose:
-            print("Projecting")
-
-        proj = self.project(prob_func)
-        proj = self.send(proj)
-        proj = proj / torch.sum(proj)
-
-        # Resample projection. This determines the number of particles to place in each cell.
-        n_samples_per_cell = resample_hist(proj, n)
-
-        # Sample within each slice of the projection axis.
-        if self.verbose:
-            print("Sampling")
-
-        x = []
-        for indices in tqdm_wrapper(np.ndindex(*proj.shape), self.verbose):
-            size = int(n_samples_per_cell[indices])
-            if size == 0:
-                continue
-
-            # Sample the projection coordinates from a uniform distribution over
-            # the current cell in the projected space.
-            y = torch.zeros((size, self.dim))
-            y = self.send(y)
-            for axis, index in enumerate(indices):
-                y[:, axis] = random_uniform(
-                    self.proj_grid_edges[axis][index],
-                    self.proj_grid_edges[axis][index + 1],
-                    size=size,
-                    device=self.device,
-                )
-                if self.noise:
-                    delta = self.proj_grid_edges[axis][index + 1] - self.proj_grid_edges[axis][index]
-                    y[:, axis] += self.noise * delta * (torch.rand(size) - 0.5)
-
-            # Set the evaluation points on the projection axis.
-            for axis, index in enumerate(indices):
-                self.eval_points[:, axis] = self.proj_grid_coords[axis][index]
-
-            # Grid sample.
-            prob = prob_func(self.eval_points)
-            prob = torch.reshape(prob, self.samp_grid_shape)
-            y[:, self.samp_axis] = sample_hist(
-                prob, self.samp_grid_edges, n=size, noise=self.noise
-            )
-
-            x.append(y)
-
-        x = torch.vstack(x)
-        x = x[torch.randperm(x.shape[0])]
-        return x
--- mentflow/utils/grid.py
+++ mentflow/utils/grid.py
@@ -2,27 +2,9 @@ import numpy as np
 import torch
 
 
-def coords_from_edges(edges):
-    """Compute bin centers from evenly spaced bin edges."""
+def coords_from_edges(edges: torch.Tensor) -> torch.Tensor:
     return 0.5 * (edges[:-1] + edges[1:])
 
 
-def get_grid_points_numpy(*coords):
-    """Return list of grid coordinates from coordinate arrays along each axis.
-
-    Parameters
-    ----------
-    coords : iterable[ndarray]
-        Coordinates along each axis of regular grid. Example: [[1, 2, 3], [0, 1, 2]].
-
-    Returns
-    -------
-    ndarray, shape (K, len(coords))
-        Coordinate array for all points in the grid. The total number of grid
-        points is `K = np.prod([len(c) for c in coords])`.
-    """
-    return np.vstack([C.ravel() for C in np.meshgrid(*coords, indexing="ij")]).T
-
-
-def get_grid_points(*coords):
+def get_grid_points(*coords: torch.Tensor) -> torch.Tensor:
     return torch.vstack([C.ravel() for C in torch.meshgrid(*coords, indexing="ij")]).T
