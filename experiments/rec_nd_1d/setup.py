592b50bca2a63cc4fe82b43bac3d6c6517cdc2bc
austin-hoover <hooveram@ornl.gov>
2024-04-13 17:19:49 -0400

    All experiments working

--- experiments/rec_nd_1d/setup.py
+++ experiments/rec_nd_1d/setup.py
@@ -17,7 +17,6 @@ from experiments.setup import train_mentflow_model
 from experiments.setup import setup_ment_model
 from experiments.setup import train_ment_model
 from experiments.setup import get_discrepancy_function
-from experiments.setup import setup_eval
 
 
 mf.train.plot.set_proplot_rc()
@@ -126,3 +125,47 @@ def setup_plot(cfg: DictConfig) -> Callable:
         device=cfg.device,
     )
     return plot
+
+
+def setup_eval(cfg: DictConfig) -> Callable:
+    """Set up eval function from config."""
+    
+    def _eval(model):         
+        # Compute distance between measured/simulated projections.
+        x_pred = model.sample(cfg.eval.size)
+        x_pred = x_pred.type(torch.float32)
+        x_pred = x_pred.to(cfg.device)
+        predictions = mf.simulate.forward(x_pred, model.transforms, model.diagnostics)    
+
+        discrepancy_function = get_discrepancy_function(cfg.eval.discrepancy)
+
+        discrepancy_vector = []
+        for y_pred, y_meas in zip(unravel(predictions), unravel(model.measurements)):
+            discrepancy = discrepancy_function(y_pred, y_meas)
+            discrepancy_vector.append(discrepancy)
+        discrepancy = sum(discrepancy_vector) / len(discrepancy_vector)
+
+        # Compute distance between true/predicted samples.   
+        distance_function = None
+        if cfg.eval.distance == "swd":
+            distance_function = mf.loss.SlicedWassersteindDistance(n_projections=50, p=2, device=cfg.device)
+
+        distance = None
+        if distance_function is not None:
+            distribution = make_distribution(cfg)
+            n_samples = cfg.eval.size
+            x_true = distribution.sample(n_samples)    
+            x_true = x_true.type(torch.float32)
+            x_true = x_true.to(cfg.device)
+            distance = distance_function(x_pred[:n_samples, :], x_true[:n_samples, :])
+            
+        # Print summary
+        print("")
+        print("disc(y_model, y_true) = {}".format(discrepancy))
+        if distance is not None:
+            print("dist(x_model, x_true) = {}".format(distance))
+        
+        results = {"discrepancy": discrepancy, "distance": distance}
+        return results
+
+    return _eval
