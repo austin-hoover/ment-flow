8e27b2cdace7c3f47b80593404c5f541c59d2192
austin-hoover <austin.michael.hoover@gmail.com>
2025-05-27 02:16:33 -0400

    Add device argument to run.sh

--- experiments/run.sh
+++ experiments/run.sh
@@ -4,15 +4,21 @@ set -x
 # This script runs all experiments for the paper. The outputs of the 
 # experiments are analyzed in the ./analysis directory.
 
+# Check if device is provided in command line argument
+if [ $# -gt 0 ]; then
+    device="$1"
+else
+    device="cpu"
+fi
+
 # Run 6D experiments
 cd ./rec_nd_1d
 rm -rf outputs
-./run_gmm.sh mps
-./run_rings.sh mps
+./run.sh $device
 cd ..
 
 # Run 2D experiments
 cd ./rec_2d/linear
 rm -rf outputs
-./run.sh mps
+./run.sh $device
 cd ../..
